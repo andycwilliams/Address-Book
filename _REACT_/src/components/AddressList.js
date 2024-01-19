6dd218244953c5027f23edbe98b3d1f0503b0a9b
Andy <awilliamscomposing@gmail.com>
2024-01-18 21:01:33 -0800

    Reinitializing app

--- _REACT_/src/components/AddressList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Address from "./Address";
-
-export default function AddressList({ addresses }) {
-  return addresses.map((address) => {
-    return <Address address={address} />;
-  });
-}
