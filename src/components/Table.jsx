c1db64cdf7125a999bef3213d6ff4078f93bfc06
Andy <awilliamscomposing@gmail.com>
2024-01-21 20:59:56 -0800

    Beginning templated components

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,107 @@
+import { useEffect } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
 const Table = () => {
+  useEffect(() => {
+    const options = {};
+    const elems = document.querySelectorAll(".collapsible");
+    const collapsibleRows = M.Collapsible.init(elems, options);
+
+    return () => {
+      collapsibleRows.forEach((e) => {
+        e.destroy();
+      });
+    };
+  }, []);
+
   return (
-    // <div className="progress">
-    //   <div className="indeterminate"></div>
-    // </div>
-    <ul className="pagination">
-      <li className="disabled">
-        <a href="#!">
-          <i className="material-icons">chevron_left</i>
-        </a>
-      </li>
-      <li className="active">
-        <a href="#!">1</a>
-      </li>
-      <li className="waves-effect">
-        <a href="#!">2</a>
-      </li>
-      <li className="waves-effect">
-        <a href="#!">3</a>
-      </li>
-      <li className="waves-effect">
-        <a href="#!">4</a>
-      </li>
-      <li className="waves-effect">
-        <a href="#!">5</a>
-      </li>
-      <li className="waves-effect">
-        <a href="#!">
-          <i className="material-icons">chevron_right</i>
-        </a>
-      </li>
-    </ul>
+    <>
+      <div className="progress">
+        {/* <div className="indeterminate"></div> */}
+      </div>
+
+      <ul className="collapsible">
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">filter_drama</i>First
+          </div>
+          <div className="collapsible-body">
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+        </li>
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">place</i>Second
+          </div>
+          <div className="collapsible-body">
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+        </li>
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">whatshot</i>Third
+          </div>
+          <div className="collapsible-body">
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+        </li>
+      </ul>
+
+      <table className="highlight ">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Upload Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Address 1</td>
+            <td>Today</td>
+            <td>Archive</td>
+          </tr>
+          <tr>
+            <td>Address 2</td>
+            <td>Yesterday</td>
+            <td>Archive</td>
+          </tr>
+          <tr>
+            <td>Address 2</td>
+            <td>Yesteryear</td>
+            <td>Archive</td>
+          </tr>
+        </tbody>
+      </table>
+      <ul className="pagination">
+        <li className="disabled">
+          <a href="#!">
+            <i className="material-icons">chevron_left</i>
+          </a>
+        </li>
+        <li className="active">
+          <a href="#!">1</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">2</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">3</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">4</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">5</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">
+            <i className="material-icons">chevron_right</i>
+          </a>
+        </li>
+      </ul>
+    </>
   );
 };
 
